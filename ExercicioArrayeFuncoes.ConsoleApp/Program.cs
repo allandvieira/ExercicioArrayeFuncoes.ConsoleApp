91d4e8f46c91c08400e529627ae1a5ad14aeb20e
Allan <Allan@DESKTOP-RA9V1AJ>
2024-03-26 13:49:29 -0300

    Add project files.

---
+++ ExercicioArrayeFuncoes.ConsoleApp/Program.cs
@@ -0,0 +1,162 @@
+﻿namespace ExercicioArrayeFuncoes.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numerosArray = new int[10] { -5, 3, 4, 5, 9, 6, 10, -2, 11, 1 };
+
+            Console.WriteLine($"O maior número da Sequência utilizando o método MAX() é: {numerosArray.Max()}");
+            Console.WriteLine($"O maior número da Sequência utilizando o método manual é: {RetornaMaiorNumeroArray(numerosArray)}");
+            Console.WriteLine();
+            Console.WriteLine($"O maior número da Sequência utilizando o método MIN() é: {numerosArray.Min()}");
+            Console.WriteLine($"O maior número da Sequência utilizando o método manual é: {RetornaMenorNumeroArray(numerosArray)}");
+            Console.WriteLine();
+            Console.WriteLine($"A media dos número da Sequência utilizando o método MIN() é: {numerosArray.Average()}");
+            Console.WriteLine($"A media dos número da Sequência utilizando o método manual é: {RetornaMediaArray(numerosArray)}");
+
+            Console.ReadLine();
+
+        }
+
+        public static int RetornaMaiorNumeroArray(int[] numeros)
+        {
+            int maiorNumero = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maiorNumero)
+                {
+                    maiorNumero = numeros[i];
+                }
+            }
+
+            return maiorNumero;
+        }
+
+        public static int RetornaMenorNumeroArray(int[] numeros)
+        {
+            int menorNumero = numeros[0];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] < menorNumero)
+                {
+                    menorNumero = numeros[i];
+                }
+            }
+
+            return menorNumero;
+        }
+
+        public static float RetornaMediaArray(int[] numeros)
+        {
+            int count = 0;
+            float media = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                count += numeros[i];
+            }
+
+            media = (float) count / numeros.Length;
+            return media;
+        }
+
+        static void TresMaiores(int[] numeros, int tamanhodovetor)
+        {
+            int maiornum = numeros[0];
+            for (int i = 0; i < tamanhodovetor; i++)
+            {
+
+                if (numeros[i] > maiornum)
+                {
+                    maiornum = numeros[i];
+                }
+            }
+
+            int aux = maiornum;
+            int count = 0;
+            Console.Write($"\nOs tres maiores numeros são: ");
+            while (true)
+            {
+                for (int i = 0; i < tamanhodovetor; i++)
+                {
+                    if (numeros[i] == aux)
+                    {
+                        //maiornum = num[i];
+                        Console.Write($"{numeros[i]}  ");
+                        aux--;
+                        count++;
+                    }
+                }
+                if (count == 3)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void NumerosNegativos(int[] numeros, int tamanhodovetor)
+        {
+            Console.Write($"\nOs numeros negativos são: ");
+            for (int i = 0; i < tamanhodovetor; i++)
+            {
+                if (numeros[i] < 0)
+                {
+                    Console.Write($"{numeros[i]}  ");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void Sequencia(int[] numeros, int tamanhodovetor)
+        {
+            Console.Write($"\nOs numeros desta sequencia são: ");
+            for (int i = 0; i < tamanhodovetor; i++)
+            {
+                Console.Write($"{numeros[i]}  ");
+            }
+            Console.ReadLine();
+        }
+
+        static int Apagar(int tamanhodovetor, int[] numeros)
+        {
+            //Imprimindo os numeros da sequencia novamente
+            Console.Write($"\nOs numeros desta sequencia são: ");
+            for (int i = 0; i < tamanhodovetor; i++)
+            {
+                Console.Write($"{numeros[i]}  ");
+            }
+
+            Console.Write($"\n\nQual numero deseja apagar? ");
+            int aux2 = Convert.ToInt32(Console.ReadLine());
+            int aux3;
+            for (int i = 0; i < tamanhodovetor; i++)
+            {
+                if (numeros[i] == aux2)
+                {
+                    aux3 = i;
+                    int aux4 = i + 1;
+                    for (int j = aux4; j < tamanhodovetor; j++)
+                    {
+                        numeros[aux3] = numeros[j];
+                        aux3++;
+                    }
+                    i--;
+                }
+            }
+            tamanhodovetor--;
+
+            //Imprimindo os numeros da sequencia novamente
+            Console.Write($"\nOs numeros desta sequencia agora são: ");
+            for (int i = 0; i < tamanhodovetor; i++)
+            {
+                Console.Write($"{numeros[i]}  ");
+            }
+            Console.ReadLine();
+            return tamanhodovetor;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Exercicio Array & Funções
+
+- Recebemos 10 valores inteiros
+
+- Permita: 
+Mostrar na Tela os valores da sequência
+Encontrar o Maior Valor da sequência
+Encontrar o Menor Valor da sequência
+Encontrar o Valor Médio da sequência
+Encontrar os 3 maiores valores da sequência
+Encontrar os valores negativos da sequência
+Remover um item da sequência
+
+- Critérios de Aceite da Atividade: 
+Deve utilizar arrays
+Deve utilizar funções
+Pode utilizar a classe “Array” do Framework
